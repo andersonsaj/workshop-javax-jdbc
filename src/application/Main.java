3fa6f87e951318313e2f1a9c9886d4a11b5102a8
andersonsaj <andersonsb09@hotmail.com>
2020-02-21 10:30:31 -0300

    MV design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -14,8 +14,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene scene = new Scene(parent);
+			//Parent parent = loader.load();
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene scene = new Scene(scrollPane);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("JavaFX application");
 			primaryStage.show();
